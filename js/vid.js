245b0adf24c674fd1e96bad8829c54f152b66972
jyamee <155151271+jyamee@users.noreply.github.com>
2024-05-18 01:15:46 +0300

    A lot of designing

--- js/vid.js
+++ js/vid.js
@@ -2,3 +2,20 @@ document.getElementById("toggleAudio").addEventListener("click", function () {
   const video = document.getElementById("bgVideo");
   video.muted = !video.muted;
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarToggle = document.getElementById("navbar-toggle");
+  const navbarMenu = document.getElementById("navbar-menu");
+
+  navbarToggle.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show");
+  });
+
+  // Animation for h1 on home page
+  const heroTitle = document.querySelector(".hero h1");
+  heroTitle.style.opacity = "0";
+  setTimeout(() => {
+    heroTitle.style.opacity = "1";
+    heroTitle.style.transition = "opacity 2s";
+  }, 500);
+});
